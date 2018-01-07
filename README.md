1d55e03e479666bce70fb19e2e0921aa5438254d
Ben Emmett <ben@Airhead.local>
2018-01-07 13:53:26 -0600

    Added capability to create new level.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RG2DLevelDesigner
 GUI based, java application for laying out Roku Game 2D levels
+
+Keyboard Shortcuts
+
+l - New level
+
+o - Open saved data.
+s - Save data.
+
+w - Write to brightscript code format
+
+
+Spritesheets
+- encode spritesheet names with the width & height of the individual cell
+	- ex: firetruck_spritesheet_w49_h36.png
--- src/control/MainController.java
+++ src/control/MainController.java
@@ -40,42 +40,66 @@ public class MainController {
 	}
 	
 	public void keyTypedHandler(KeyEvent e){
-		System.out.println("MC:Key Typed " + e.getKeyChar());
-
-		if(e.getKeyChar() == 's'){
-			
-			System.out.println("Saving Data.");
-			db.saveDatabase(db.GamePath + "Data.bin");
-
-		} else if (e.getKeyChar() == 'o') {
-			
-			db.loadDatabase(db.GamePath + "Data.bin");
-
-			//Draw first level by default
-			// TODO get level selected my level window or, set level window selection to 0.
-			lwin.level = db.LevelArray.get(0);
-			
-			//UPdate views
-			lwin.DrawLevel();
-			swin.DisplaySpriteList();
-			vwin.DisplayLevelList();
-			
-		} else {
-			System.out.println("Writing Code.");
-			try {
-				LBCodeGen.GenCode();
-			} catch (IOException e1) {
-				System.out.println("Failed to generate brightscript code!");
-				e1.printStackTrace();
+		//System.out.println("MC:Key Typed " + e.getKeyChar());
+		
+		switch(e.getKeyChar()){
+			case 'l':{
+				int nextLevel = db.LevelArray.size() + 1;
+				int il = db.CreateNewLevel("Level_0" + nextLevel, nextLevel);
+				
+				lwin.level = db.LevelArray.get(il);
+				vwin.activeLevel = il;
+				
+				swin.DisplaySpriteList();
+				vwin.DisplayLevelList();
+				break;
+				
 			}
+			case 'o':{
+
+				db.loadDatabase(db.GamePath + "Data.bin");
+
+				//Draw first level by default
+				// TODO get level selected my level window or, set level window selection to 0.
+				lwin.level = db.LevelArray.get(0);
+				
+				//UPdate views
+				lwin.DrawLevel();
+				swin.DisplaySpriteList();
+				vwin.DisplayLevelList();
+				break;
+				
+			} 
+			case 's':{
+	
+					System.out.println("Saving Data.");
+					db.saveDatabase(db.GamePath + "Data.bin");	
+					break;
+			} 
+			case 'w':{
+
+				System.out.println("Writing Code.");
+				try {
+					LBCodeGen.GenCode();
+				} catch (IOException e1) {
+					System.out.println("Failed to generate brightscript code!");
+					e1.printStackTrace();
+				}
+				break;
+			}
+			default: {
+				System.out.println("MC: Warning: " + e.getKeyChar() + " Key pressed. Doing nothing about it.");
+			}
+		
 		}
+
 	}
 	public void levelSelectionChangeHandler(ListSelectionEvent e) {
 
 		System.out.println("MC: Level selection changed");
 		
 		//Update level
-		if(vwin.activeLevel > 0){
+		if(vwin.activeLevel >= 0){
 			lwin.level = db.LevelArray.get(vwin.activeLevel);
 			lwin.DrawLevel();
 		}
